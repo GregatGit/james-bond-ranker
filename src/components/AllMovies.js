f09ce07a0310353ed4dc3a17cfbc2e6f826bde7d
gregatgit <gregdd@outlook.com>
2019-08-12 22:53:22 +1000

    use  material UI makeStyles to adjust img styles

--- src/components/AllMovies.js
+++ src/components/AllMovies.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { makeStyles } from '@material-ui/core/styles'
-import GridList from '@material-ui/core/GridList'
-import GridListTile from '@material-ui/core/GridListTile'
-import GridListTileBar from '@material-ui/core/GridListTileBar'
-import ListSubheader from '@material-ui/core/ListSubheader'
-import IconButton from '@material-ui/core/IconButton'
+import { makeStyles, GridList, GridListTile, GridListTileBar, ListSubheader, IconButton } from '@material-ui/core/'
 import InfoIcon from '@material-ui/icons/Info'
 import { movieSelected } from '../actions'
 
--- src/components/MovieChosen.js
+++ src/components/MovieChosen.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/'
 
-const MovieChosen = ({chosen}) => {
+const useStyles = makeStyles(theme => ({
+  imgLarge: {
+    maxWidth: '100%',
+    maxHeight: (window.innerHeight / 100) * 90,
+  },
+}))
+
+const MovieChosen = ({ chosen }) => {
+  const styles = useStyles()
   const { imgLarge, title } = chosen
-  const maxH = window.innerHeight / 100 * 90
+  const maxH = (window.innerHeight / 100) * 90
   return (
     <div>
-      {(typeof chosen === 'string') ? chosen : (
-        <img style={{maxWidth: '100%', maxHeight: maxH}} src={imgLarge} alt={title} />
-      ) }
+      {typeof chosen === 'string' ? (
+        chosen
+      ) : (
+        <img className={styles.imgLarge} src={imgLarge} alt={title} />
+      )}
     </div>
   )
 }
